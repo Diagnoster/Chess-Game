939d189333a55ea52589f2be9d9a259d500ab666
James Rovel Barbosa <diagjames@gmail.com>
2021-05-21 09:49:45 -0300

    create tabuleiro

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using tabuleiro;
 
 namespace chess_console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ chess-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Tabuleiro {
+        // xadrez é 8x8
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas; // matriz de peças
+
+        public Tabuleiro (int linhas, int colunas) { 
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas]; // instancia matriz de peca e defini quantas linhas e colunas ele vai ter
+        }
+
+    }
+}
